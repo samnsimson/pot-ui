9516ac0a689fbbe79e019f1f3f97409a38c002ce
samnsimson <samnishanth01@gmail.com>
2025-02-04 01:51:49 -0500

    remove unused

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,4 @@
 import { clsx, type ClassValue } from "clsx";
-import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
